6e69b125531cef156e0ca03ab42b5c5986bedfda
Lord Soth <allhailsoth@gmail.com>
2025-11-05 02:35:22 -0500

    Revert "first push"
    This reverts commit 788769d7a1eb52d05984572ebc0620747b1d7ede.

--- backend/server.js
+++
@@ -1,7 +0,0 @@
-import app from './app.js'
-
-const PORT = 4200
-
-app.listen(PORT, () => {
-    console.log(`listening on localhost:${PORT}`)
-})
--- frontend/index.js
+++
@@ -1,5 +0,0 @@
-const start = document.getElementById('start')
-
-start.addEventListener('mousedown', () => {
-    window.location.href = './preview.html'
-})
